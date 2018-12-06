64caf176fbcb3cb647e5c84f53bd0030732f2da2
Shaun McArthur <shamca@bell.net>
2018-12-06 15:11:14 -0500

    added edit method

--- app.js
+++ app.js
@@ -6,7 +6,17 @@ var app = new Vue({
                 template: `
                          <div class="ui segment task"
                              v-bind:class="task.completed ? 'done': 'todo' ">
-                                {{ task.id}} {{ task.name }}: {{ task.description }}: Completed?: {{ task.completed}}
+                             <div class="ui grid">
+                                    <div class="left floated twelve wide column">
+                                        <div class="ui checkbox">
+                                        <input type="checkbox" name="task" v-on:click="app.toggleDone($event, task.id)" :checked="task.completed" >
+                                        <label> {{ task.name }} <span class="description"> {{ task.description }}</span></label>
+                                    </div>
+                                </div>
+                             <div class="right floated three wide column">
+                            <i class="icon pencil blue" alt="Edit" v-on:click="app.editTask($event, task.id)"></i>
+                             <i class="icon trash red" alt="Delete" v-on:click="app.deleteTask($event, task.id)"></i>
+                             </div>
                         </div>
                         `
       }
@@ -27,6 +37,32 @@ var app = new Vue({
             },
             todoTasks: function() {
                 return this.tasks.filter(item => item.completed == false);
+            }
+        },
+        methods: {
+            toggleDone: function(event, id) {
+                event.stopImmediatePropagation();
+
+                let task = this.tasks.find(item=> item.id == id);
+                if (task) {
+                    task.completed = !task.completed;
+                    console.log('task toggled');
+                }
+            },
+            deleteTask: function(event, id) {
+                event.stopImmediatePropagation();
+
+                let taskIndex = this.tasks.findIndex(item => item.id = id);
+
+                if (taskIndex > -1) {
+                    this.$delete(this.tasks, taskIndex);
+                    console.log('deleted : ' + id);
+                }
+
+            },
+            editTask: function(event, id) {
+                let taskIndex = this.tasks.findIndex(item => item.id = id);
+                console.log("clicked edit");
             }
         }
 })
