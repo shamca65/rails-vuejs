27c8d834c93e93b91c87c5edd7e9711d90b1c83b
Shaun McArthur <shamca@bell.net>
2018-12-06 12:20:01 -0500

    working finally

--- app.js
+++ app.js
@@ -2,19 +2,11 @@ var app = new Vue({
     el: '#app',
     data: {
         tasks: [
-            {id:1, name: 'Todo 1', description: 'This is number 1', completed: false }
-            {id:2, name: 'Todo 2', description: 'This is number 2', completed: false }
-            {id:3, name: 'Todo 3', description: 'This is number 3', completed: false }
-            {id:4, name: 'Todo 4', description: 'This is number 4', completed: false }
+            { id: 1, name: 'Todo 1', description: 'This is a todo', completed: false },
+            { id: 2, name: 'Todo 2', description: 'This is another todo', completed: true },
+            { id: 3, name: 'Three', description: 'This is a compplete todo', completed: true },
+            { id: 4, name: 'Four', description: 'This is another complete todo', completed: true }
         ],
-        computed: {
-            completedTasks: function() {
-                return this.tasks.filter( item => item.completed == true );
-            },
-            todoTasks: function() {
-                return this.tasks.filter( item => item.completed == false);
-            }
-        },
-        message: 'hello there'
-    }
-})
+        message: 'Hello World!'
+    },
+});
