9597d9db93ed9cb57e0f125f181b011306f344a1
Shaun McArthur <shamca@bell.net>
2018-12-06 12:33:47 -0500

    added computed properties

--- app.js
+++ app.js
@@ -3,10 +3,19 @@ var app = new Vue({
     data: {
         tasks: [
             { id: 1, name: 'Todo 1', description: 'This is a todo', completed: false },
-            { id: 2, name: 'Todo 2', description: 'This is another todo', completed: true },
+            { id: 2, name: 'Todo 2', description: 'This is another todo', completed: false },
             { id: 3, name: 'Three', description: 'This is a compplete todo', completed: true },
             { id: 4, name: 'Four', description: 'This is another complete todo', completed: true }
         ],
         message: 'Hello World!'
-    },
-});
+        },
+        computed: {
+            completedTasks: function() {
+                return this.tasks.filter(item => item.completed == true);
+
+            },
+            todoTasks: function() {
+                return this.tasks.filter(item => item.completed == false);
+            }
+        }
+})
